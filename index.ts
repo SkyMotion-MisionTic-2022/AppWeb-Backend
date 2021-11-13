7f698920f5e3538fc1ef607dcd2e58177e468b4d
JuanVaquiro <jvaquiro23@itfip.edu.co>
2021-11-13 14:14:42 -0500

    creacion de modulos y usuario

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
 import conectarDB from "./db/db";
+import { UserModel } from './models/user';
+import { Enum_Rol } from "./models/enums";
 
 const main = async () => {
     await conectarDB ();
+
+    //CREAR USUARIO
+    UserModel.create({
+        correo:"skymotion99@.com",
+        identificacion:"1070121011",
+        nombre: "sky1",
+        apellido: "motion1",
+        rol: Enum_Rol.administrador,
+    })
+    .then((u) =>{
+         console.log('usuario creado', u);
+    })  
+    .catch((e) =>{
+        console.error('Error creando el usuario', e);
+    });
+
+    //OBTENER LOS USUARIOS
+    // await UserModel.find()  //Dentro del fn find se puede filtrar y hacer la consulta
+    //     .then((u) =>{
+    //         console.log('usuario', u);
+    //     })
+    //     .catch((e) =>{
+    //         console.error('error obtenido los usuarios', e)
+    //     });
 };
 
 main ();
