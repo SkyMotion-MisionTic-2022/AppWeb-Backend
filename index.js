b5511b9161a38abf847ff2f84aa75907fa86f793
paolaAndrea5 <you@example.com>
2021-12-17 01:14:47 -0500

    despliegue

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.listen({ port: process.env.PORT || 8080 }, async () => {
+app.listen({ port: process.env.PORT || 4000 }, async () => {
   await conectarBD();
   await server.start();
 
